da1a1d33648a75c27bd6bfc94028bf9d1ef1c518
GoncaloArnauth <goncaloarnauth@outlook.com>
2016-09-30 12:36:15 +0100

    test conection and small bugs fixes

--- src/test/java/pt/isel/ls/ConnectivityTests.java
+++ src/test/java/pt/isel/ls/ConnectivityTests.java
@@ -1,6 +1,7 @@
 package pt.isel.ls;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import org.junit.Test;
 
 import java.sql.*;
@@ -10,10 +11,19 @@ import java.util.Properties;
 import static org.junit.Assert.*;
 
 public class ConnectivityTests {
-    final String url="jdbc:sqlserver://localhost;";
     private final SQLServerDataSource src = new SQLServerDataSource();
- //   final String user="jdbcuser";
- //   final String password="jdbcuser";
+
+    @Test(expected=SQLServerException.class)
+    public void teste_error_connect()  throws SQLServerException{
+        Connection con = null;
+
+            Map<String, String> env = System.getenv();
+
+            src.setServerName(env.get("ausdjadaks"));
+            src.setUser(env.get("ssa"));
+            src.setPassword(env.get("hfgdf"));
+            con = src.getConnection();
+    }
 
     @Test
     public void connectivity() throws SQLException {
@@ -28,29 +38,35 @@ public class ConnectivityTests {
         con=src.getConnection();
         con.setAutoCommit(false);
 
-        PreparedStatement insert1 = con.prepareStatement("insert into student values ('Pedro',5)");
+        PreparedStatement confirm1 = con.prepareStatement("IF object_id('studentTest', 'U') is not null\n" +
+                "        begin\n" +
+                "        drop table studentTest\n" +
+                "        end");
+        confirm1.execute();
+
+        PreparedStatement create1 = con.prepareStatement( "create table studentTest(\n" +
+                "\tname varchar(10) ,\n" +
+                "\tid int primary Key\n" +
+                " )");
+        create1.execute();
+
+        PreparedStatement insert1 = con.prepareStatement("insert into studentTest values ('Pedro',5)");
         insert1.executeUpdate();
-        PreparedStatement st1 = con.prepareStatement("select * from student where id=5");
+        PreparedStatement st1 = con.prepareStatement("select * from studentTest where id=5");
 
         ResultSet rs1 = st1.executeQuery();
         rs1.next();
         assertEquals("Pedro", rs1.getString(1));
 
-        PreparedStatement update = con.prepareStatement("update student set name='Sipriano' where id=5 ");
+        PreparedStatement update = con.prepareStatement("update studentTest set name='Sipriano' where id=5 ");
         update.executeUpdate();
 
-        PreparedStatement st2 = con.prepareStatement("select * from student where id=5");
+        PreparedStatement st2 = con.prepareStatement("select * from studentTest where id=5");
         ResultSet rs2 = st2.executeQuery();
 
         rs2.next();
         assertEquals("Sipriano", rs2.getString(1));
         con.rollback();
 
-
-            /*  ResultSet result = statement.executeQuery();
-            while(result.next()){
-                System.out.println(result.getString(1) + ' ' + result.getInt(2));
-            }
-            */
     }
 }
