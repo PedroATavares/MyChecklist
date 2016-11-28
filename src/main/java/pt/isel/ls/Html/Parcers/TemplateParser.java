b342ec763b2ffc34caad15a805de96819a26148f
GoncaloArnauth <goncaloarnauth@outlook.com>
2016-11-28 20:04:57 +0000

    added the connection in the html template with cheklist

--- src/main/java/pt/isel/ls/Html/Parcers/TemplateParser.java
+++ src/main/java/pt/isel/ls/Html/Parcers/TemplateParser.java
@@ -7,25 +7,33 @@ import java.util.List;
 
 import static pt.isel.ls.Html.Source.HtmlSupplier.*;
 
-public class TemplateParser implements HtmlParser<Template> {
+public class TemplateParser implements HtmlParser<List<Template>> {
 
     @Override
-    public String supply(Template source) {
-        return html().with(body().with(
+    public String supply(List<Template> source) {
+        if(source==null || source.isEmpty()) return paragraph().withText("No Templates To Show.").toHtml();
+        HtmlElement table = table().with(
+                tr().with(
                 h3().withText("TEMPLATE"),
-                paragraph().withText("Id: " + source.id ),
-                paragraph().withText("Name: " + source.name),
-                paragraph().withText("Description: " + source.description)
-        )).toHtml();
-    }
+                th().withText("Id" ),
+                th().withText("Name"),
+                th().withText("Description")
+                )
+        ).withAttribute(attribute("style", "width:50%"), attribute("border", "1"));
 
+        for (Template c: source) {
+            table.with(makeRow(c));
+        }
+        return  html().with(body().with(table,h3().with(hyperlink("CheckLists","/checklists"))
+        )).toHtml();
+
+    }
 
     private static HtmlElement makeRow(Template t) {
         return tr().with(
                 td().withText(String.valueOf(t.id)),
                 td().withText(String.valueOf(t.name)),
                 td().withText(t.description)
-
         );
     }
 }
